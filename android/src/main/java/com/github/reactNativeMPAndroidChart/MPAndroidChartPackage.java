4eab8cb67562c89a23435feee83fa03962e8068e
mskec <martin.skec@gmail.com>
2016-04-05 21:56:24 +0200

    Initial RN Android module

---
+++ android/src/main/java/com/github/reactNativeMPAndroidChart/MPAndroidChartPackage.java
@@ -0,0 +1,32 @@
+package com.github.reactNativeMPAndroidChart;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MPAndroidChartPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(
+
+        );
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+}
